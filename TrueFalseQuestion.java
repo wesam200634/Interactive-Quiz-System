dec547798191fa4ce9b7025ca5118fef1e03e9cb
yazanishtay30-alt <yazanishtay30@gmail.com>
2026-04-04 00:16:05 +0300

    Update TrueFalseQuestion.java

--- TrueFalseQuestion.java
+++ TrueFalseQuestion.java
@@ -5,6 +5,7 @@ package quizproject;
 public class TrueFalseQuestion extends Question {
     @Override//this helps us know that this method is inherited from the father but this child change the way it works on its own behaviour
     public boolean checkAnswer(Answer a){
+        //we used equals function to compare between strings instead of memory
         return correctAnswer.equals(a.getResponse());
         
     }}
