986b7b61ce359ee9d77360e6204db879c0f8d822
yazanishtay30-alt <yazanishtay30@gmail.com>
2026-04-05 23:19:59 +0300

    Update FillTheBlankQuestion.java

--- FillTheBlankQuestion.java
+++ FillTheBlankQuestion.java
@@ -5,6 +5,6 @@ package quizproject;
 public class FillTheBlankQuestion extends Question {
     @Override
     public boolean checkAnswer(Answer a){
-         return correctAnswer.toLowerCase().equals(a.getResponse());
+         return correctAnswer.toLowerCase().equals(a.getResponse()); //making sure the strings in Lowercase always
     }
 }
